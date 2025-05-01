f2afb6092fa9fc49ae74d8468249b03a3e6685c9
Anirudh-tech-77 <anirudhh1232024@gmail.com>
2025-05-01 21:51:49 +0530

    Update afor.java

--- afor.java
+++ afor.java
@@ -1,7 +1,7 @@
 public class afor {
     public static void main(String args[]){
         for(int i=1; i<=10; i++){
-            System.out.println("hello world");
+            System.out.println(" world");
         }
     }
     
